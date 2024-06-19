3a8114f0844f4e2da5d7df47e08e0fc82031cb65
Agneya Pathare <61740206+agneya-1402@users.noreply.github.com>
2024-06-19 08:39:12 +0530

    Update ColorDetection.py

--- ColorDetection.py
+++ ColorDetection.py
@@ -1,23 +1,21 @@
 import numpy as np
 import cv2
 
-
+# turn on cam
 webcam = cv2.VideoCapture(0)
 
 
 while (1):
     _, imageFrame = webcam.read()
 
-    # Convert the imageFrame in
-    # BGR(RGB color space) to
-    # HSV(hue-saturation-value)
-    # color space
+    # Convert BGR to HSV colorspace
     hsvFrame = cv2.cvtColor(imageFrame, cv2.COLOR_BGR2HSV)
 
-    # Set range for red color and
-    # define mask
+    # Set range for red color 
     red_lower = np.array([136, 87, 111], np.uint8)
     red_upper = np.array([180, 255, 255], np.uint8)
+    
+    # define mask
     red_mask = cv2.inRange(hsvFrame, red_lower, red_upper)
 
     #green color
@@ -34,25 +32,20 @@ while (1):
     # to detect only that particular color
     kernal = np.ones((5, 5), "uint8")
 
-    # For red color
+    # red color
     red_mask = cv2.dilate(red_mask, kernal)
-    res_red = cv2.bitwise_and(imageFrame, imageFrame,
-                              mask=red_mask)
+    res_red = cv2.bitwise_and(imageFrame, imageFrame, mask=red_mask)
 
-    # For green color
+    # green color
     green_mask = cv2.dilate(green_mask, kernal)
-    res_green = cv2.bitwise_and(imageFrame, imageFrame,
-                                mask=green_mask)
+    res_green = cv2.bitwise_and(imageFrame, imageFrame, mask=green_mask)
 
-    # For blue color
+    # blue color
     blue_mask = cv2.dilate(blue_mask, kernal)
-    res_blue = cv2.bitwise_and(imageFrame, imageFrame,
-                               mask=blue_mask)
+    res_blue = cv2.bitwise_and(imageFrame, imageFrame, mask=blue_mask)
 
     # Creating contour to track red color
-    contours, hierarchy = cv2.findContours(red_mask,
-                                           cv2.RETR_TREE,
-                                           cv2.CHAIN_APPROX_SIMPLE)
+    contours, hierarchy = cv2.findContours(red_mask, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
 
     for pic, contour in enumerate(contours):
         area = cv2.contourArea(contour)
@@ -66,7 +59,7 @@ while (1):
                         cv2.FONT_HERSHEY_SIMPLEX, 1.0,
                         (0, 0, 255))
 
-            # Creating contour to track green color
+    # Creating contour to track green color
     contours, hierarchy = cv2.findContours(green_mask,
                                            cv2.RETR_TREE,
                                            cv2.CHAIN_APPROX_SIMPLE)
@@ -83,7 +76,7 @@ while (1):
                         cv2.FONT_HERSHEY_SIMPLEX,
                         1.0, (0, 255, 0))
 
-            # Creating contour to track blue color
+    # Creating contour to track blue color
     contours, hierarchy = cv2.findContours(blue_mask,
                                            cv2.RETR_TREE,
                                            cv2.CHAIN_APPROX_SIMPLE)
@@ -99,7 +92,9 @@ while (1):
                         cv2.FONT_HERSHEY_SIMPLEX,
                         1.0, (255, 0, 0))
 
-    cv2.imshow("Multiple Color Detection in Real-TIme", imageFrame)
+
+    # final run
+    cv2.imshow("Color Detection", imageFrame)
     if cv2.waitKey(10) & 0xFF == ord('q'):
         webcam.release()
         cv2.destroyAllWindows()
